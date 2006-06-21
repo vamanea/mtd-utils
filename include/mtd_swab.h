8e181c4a907c3c97c61a1dfc4f5c2c58793132f5
Josh Boyer <jwboyer@gmail.com>
2006-06-21 08:08:25 -0500

    Fix swab32 to use standard types
    Signed-off-by: Josh Boyer <jwboyer@gmail.com>

--- include/mtd_swab.h
+++ include/mtd_swab.h
@@ -9,10 +9,10 @@
                 (((uint16_t)(x) & (uint16_t)0xff00U) >> 8) ))
 #define swab32(x) \
         ((uint32_t)( \
-                (((uint32_t)(x) & (__u32)0x000000ffUL) << 24) | \
-                (((uint32_t)(x) & (__u32)0x0000ff00UL) <<  8) | \
-                (((uint32_t)(x) & (__u32)0x00ff0000UL) >>  8) | \
-                (((uint32_t)(x) & (__u32)0xff000000UL) >> 24) ))
+                (((uint32_t)(x) & (uint32_t)0x000000ffUL) << 24) | \
+                (((uint32_t)(x) & (uint32_t)0x0000ff00UL) <<  8) | \
+                (((uint32_t)(x) & (uint32_t)0x00ff0000UL) >>  8) | \
+                (((uint32_t)(x) & (uint32_t)0xff000000UL) >> 24) ))
 
 #if __BYTE_ORDER == __BIG_ENDIAN
 #define cpu_to_le16(x) ({ uint16_t _x = x; swab16(_x); })
