c4c6a59eb11db9b38c86bc26575a23dbac2e616f
linux-mtd-owner@lists.infradead.org <linux-mtd-owner@lists.infradead.org>
2009-02-11 04:05:39 -0500

    Artem Bityutskiy said once: |I mean, IMO most of these warnings are not really sane, and cleaning |that up just adds mess - you stop understanding why is something signed |or unsigned  :-)
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- common.mk
+++ common.mk
@@ -2,7 +2,7 @@ CC := $(CROSS)gcc
 AR := $(CROSS)ar
 RANLIB := $(CROSS)ranlib
 CFLAGS ?= -O2 -g
-CFLAGS += -Wall -Wwrite-strings -W -D_FILE_OFFSET_BITS=64
+CFLAGS += -Wall -Wextra -Wwrite-strings -Wno-sign-compare -D_FILE_OFFSET_BITS=64
 
 DESTDIR ?= /usr/local
 PREFIX=/usr
