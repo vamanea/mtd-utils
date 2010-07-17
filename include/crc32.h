2d40ed7b415bcb6a358c9f221160c4a419f01f19
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2010-07-17 09:43:08 +0300

    libs: make crc32 and fec to be libraries
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- include/crc32.h
+++ include/crc32.h
@@ -7,16 +7,7 @@
 
 #include <stdint.h>
 
-extern const uint32_t crc32_table[256];
-
-/* Return a 32-bit CRC of the contents of the buffer. */
-static inline uint32_t crc32(uint32_t val, const void *ss, int len)
-{
-	const unsigned char *s = ss;
-
-	while (--len >= 0)
-		val = crc32_table[(val ^ *s++) & 0xff] ^ (val >> 8);
-	return val;
-}
+/* Return a 32-bit CRC of the contents of the buffer */
+extern uint32_t crc32(uint32_t val, const void *ss, int len);
 
 #endif /* __CRC32_H__ */
--- lib/libcrc32.c
+++ lib/libcrc32.c
@@ -39,7 +39,7 @@
 
 #include <stdint.h>
 
-const uint32_t crc32_table[256] = {
+const static uint32_t crc32_table[256] = {
 	0x00000000L, 0x77073096L, 0xee0e612cL, 0x990951baL, 0x076dc419L,
 	0x706af48fL, 0xe963a535L, 0x9e6495a3L, 0x0edb8832L, 0x79dcb8a4L,
 	0xe0d5e91eL, 0x97d2d988L, 0x09b64c2bL, 0x7eb17cbdL, 0xe7b82d07L,
@@ -93,3 +93,12 @@ const uint32_t crc32_table[256] = {
 	0x5d681b02L, 0x2a6f2b94L, 0xb40bbe37L, 0xc30c8ea1L, 0x5a05df1bL,
 	0x2d02ef8dL
 };
+
+uint32_t crc32(uint32_t val, const void *ss, int len)
+{
+	const unsigned char *s = ss;
+
+	while (--len >= 0)
+		val = crc32_table[(val ^ *s++) & 0xff] ^ (val >> 8);
+	return val;
+}
